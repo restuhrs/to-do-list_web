9ffc918bce8e647f6beafb62dee524db2e93de62
Restu <restukharisma858@gmail.com>
2025-03-18 21:17:04 +0700

    update bg

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #14C38E;">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000080;">
         <div class="container">
             <a class="navbar-brand" href="#">To Do List App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
